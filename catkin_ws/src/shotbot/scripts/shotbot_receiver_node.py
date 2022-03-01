e8ae0b245972bcd6388362a6afb535239b0e4520
the-sutt <github@roman55.de>
2022-03-01 11:20:19 +0100

    working on scripts

--- catkin_ws/src/shotbot/scripts/shotbot_receiver_node.py
+++ catkin_ws/src/shotbot/scripts/shotbot_receiver_node.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import rospy
 from std_msgs.msg import String
-from PositionMessage.srv import PositionMessage, PositionMessageResponse
+#from PositionMessage.srv import PositionMessage, PositionMessageResponse
 
 
 def receiveHandle(data):
     rospy.loginfo("ShotBot Receiver: new target set: %s", data.data)
     try:
-        serviceHandler = rospy.ServiceProxy('shotbot', PositionMessage)
+#        serviceHandler = rospy.ServiceProxy('shotbot', PositionMessage)
         response = serviceHandler(data.data)
 
     except rospy.ServiceException as ex:
@@ -15,13 +15,13 @@ def receiveHandle(data):
 
 
 def shotbotReceiver():
-    rospy.loginfo("ShotBot Receiver: starting")
     # init the node to communicate with other nodes
     rospy.init_node('shotbot_receiver')
+    rospy.loginfo("ShotBot Receiver: starting")
     # wait for service to be constructed
     rospy.wait_for_service('shotbot')
     # subscribe to topic to listen for incoming commands
-    rospy.Subscribeer('shotbot_gotoTarget', String, receiveHandle)
+    rospy.Subscriber('shotbot_gotoTarget', String, receiveHandle)
     # keep "thread" running
     rospy.spin()
 
